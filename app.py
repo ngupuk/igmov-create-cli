ee9c618b7f872031151ef495397d23e6bc5ace2e
Muhammad Wafa <wafax.4@gmail.com>
2020-07-03 14:45:12 +0700

    remove instapy

--- app.py
+++ app.py
@@ -2,13 +2,9 @@ from igmov.template import ig
 from igmov.dl import getFile
 import os
 import os.path
-import instapy_cli
 
 title = "Test Video"
 bgKeyword = "nature"
-igCaption = """
-Video ini di upload dari github.
-"""
 pathVideo = "results/sample.mp4"
 
 # download audio file
@@ -28,11 +24,3 @@ if(not os.path.isdir('results')):
   os.makedirs('results')
 
 base.makeVideo("sample.ogg", pathVideo)
-
-# upload to ig
-username = 'ngupuk.id'
-password = os.environ['IG_PASSWORD']
-
-with instapy_cli.client(username, password) as cli:
-  cli.upload(pathVideo, igCaption)
-
