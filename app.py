b715aab1376c88a326d93c81ab6e395fd568c549
Muhammad Wafa <wafax.4@gmail.com>
2020-07-03 13:02:49 +0700

    Setup artifact and test

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
-print("Hello World!")
+from igmov.template import ig
+from igmov.dl import getFile
+import os
+import os.path
+
+title = "Test Video"
+bgKeyword = "nature"
+
+# download audio file
+getFile(
+  "https://mwafa.github.io/images/sample.ogg",
+  "sample.ogg"
+)
+
+base = (ig.template1()
+        .useRandomBg()
+        .showSpotify()
+        .useInstagramLogo()
+        .useNgupukLogo())
+
+if(not os.path.isdir('results')):
+  os.makedirs('results')
+
+base.makeVideo("sample.ogg", "results/sample_result.mp4")
