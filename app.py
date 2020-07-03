4420f4786d59743363c6046db4b71cae62b54e85
Muhammad Wafa <wafax.4@gmail.com>
2020-07-03 14:30:16 +0700

    test upload to ig

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from igmov.template import ig
 from igmov.dl import getFile
 import os
 import os.path
+import instapy_cli
 
 title = "Test Video"
 bgKeyword = "nature"
+igCaption = """
+Video ini di upload dari github.
+"""
+pathVideo = "results/sample.mp4"
 
 # download audio file
 getFile(
@@ -22,4 +27,11 @@ base = (ig.template1()
 if(not os.path.isdir('results')):
   os.makedirs('results')
 
-base.makeVideo("sample.ogg", "results/sample.mp4")
+base.makeVideo("sample.ogg", pathVideo)
+
+# upload to ig
+username = 'ngupuk.id'
+password = os.environ['IG_PASSWORD']
+
+with instapy_cli.client(username, password) as cli:
+  cli.upload(pathVideo, igCaption)
